b9912cf0143b5df7ad6b34c01037714328ca384e
serginh023 <psergio.farias@gmail.com>
2020-06-30 12:50:23 -0300

    Terminadas animações, rotação e salto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            anim.SetBool("isMagic", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.up * 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.up * -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-.1f, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(.1f, 0, 0);
+        }
+    }
+
+    void StopJumping()
+    {
+        anim.SetBool("isJumping", false);
+    }
+
+    void Stopmagic()
+    {
+        anim.SetBool("isMagic", false);
     }
 }
