10452b88d86caf47d0227c943d45563d1141d37b
serginh023 <psergio.farias@gmail.com>
2020-06-30 21:29:53 -0300

    Terminada geração procedural do mapa, utilizando Object Pool

---
+++ Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    GameObject m_DummyTraveller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_DummyTraveller = new GameObject("dummy");
+
+        for(int i = 0; i < 20; i++)
+        {
+            GameObject go = Pool.singleton.GetRandom();
+
+            if (go == null) return;
+
+            go.SetActive(true);
+            go.transform.position = m_DummyTraveller.transform.position;
+            go.transform.rotation = m_DummyTraveller.transform.rotation;
+
+            if (go.tag == "stairsUp")
+            {
+                m_DummyTraveller.transform.Translate(0, 5, 0);
+            }
+            else if(go.tag == "stairsDown")
+            {
+                m_DummyTraveller.transform.Translate(0, -5, 0);
+                go.transform.Rotate( new Vector3(0, 180, 0) );
+                go.transform.position = m_DummyTraveller.transform.position;
+            }
+            else if (go.tag == "platformTSection")
+            {
+                if(Random.Range(0, 2) == 0)
+                    m_DummyTraveller.transform.Rotate( new Vector3(0, 90, 0) );
+                else
+                    m_DummyTraveller.transform.Rotate(new Vector3(0, -90, 0));
+
+                m_DummyTraveller.transform.Translate(Vector3.forward * -10);//avança
+            }
+
+            m_DummyTraveller.transform.Translate(Vector3.forward * -10);//avança
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class ItemPool
+{
+    public GameObject prefab;
+    public int amount;
+    public bool expandable;
+}
+
+public static class Utils
+{
+    public static System.Random r = new System.Random();
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while(n > 1)
+        {
+            n--;
+            int k = r.Next(n+1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
+
+public class Pool : MonoBehaviour
+{
+    public static Pool singleton;
+
+    public List<ItemPool> items;
+
+    public List<GameObject> pooledItems;
+
+    private void Awake()
+    {
+        singleton = this;
+
+        pooledItems = new List<GameObject>();
+        foreach (ItemPool item in items)
+        {
+            for (int i = 0; i < item.amount; i++)
+            {
+                GameObject go = Instantiate(item.prefab);
+                go.SetActive(false);
+                pooledItems.Add(go);
+            }
+        }
+    }
+
+    public GameObject GetRandom()
+    {
+        Utils.Shuffle(pooledItems);
+
+        for(int i = 0; i < pooledItems.Count; i++)
+            if (!pooledItems[i].activeInHierarchy)
+                return pooledItems[i];
+
+
+        foreach(ItemPool item in items)
+        {
+            if (item.expandable)
+            {
+                GameObject go = Instantiate(item.prefab);
+                go.SetActive(false);
+                pooledItems.Add(go);
+                return go;
+            }
+        }
+
+        return null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
